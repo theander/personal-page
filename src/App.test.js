ac08874cf864bc501e9e9e539874ca92170587ad
theander <aclaudino@gmail.com>
2022-01-30 17:48:32 -0300

    Added image and icons

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import {act} from "react-dom/test-utils"
+
 import {render} from "react-dom"
 import App from "./App"
-let container =null;
+
 
 it("renders without crashing",()=>{
 const div =document.createElement("div");
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,31 @@
+
+let path = process.env.PUBLIC_URL;
+let image = "/images/UcYBL5V0xWQ-unsplash.jpg"
 const Content = () =>{
   return (
-<div>
-  <h1>Content</h1>
+<div className="content">
+  <h1>Full Stack Developer</h1>
+
+  <img src={path+image} alt="image1"></img>
+
+
 <section>
 <h1>Hard skills</h1>
+<i class="fas fa-database fa-10x"></i>
+<i class="fab fa-js-square fa-10x"></i>
+<i class="fab fa-react fa-10x"></i>
+<i class="fab fa-node fa-10x"></i>
 
+<i class="fab fa-java fa-10x"></i>
+<i class="fab fa-git-alt fa-10x"></i>
 </section>
 
 <section>
 <h1>Soft skills</h1>
+<p>Fast Learning</p>
+<p>Problem solving</p>
+<p>Patiente</p>
+<p>Communication</p>
 
 </section>
 <section>
@@ -21,24 +38,6 @@ const Content = () =>{
 </section>
 <section>
 <h1>Portifolio</h1>
-
-</section>
-
-
-
-
-<section class="slider">
-    <input name='slide' type="radio" />
-  <input name='slide' type="radio" checked/>
-<input name='slide' type="radio"/>
-    <input name='slide' type="radio"/>
-
-    <div class="slider-content">
-        <div class="slider-item">1</div>
-        <div class="slider-item">2</div>
-        <div class="slider-item">3</div>
-        <div class="slider-item">4</div>
-    </div>
 
 </section>
 
